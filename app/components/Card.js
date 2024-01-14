2512e4a861577e891ef0a2f224d788026fdb90e7
NicolasKmbr <braun.nicolasbraun@gmail.com>
2024-01-13 20:09:03 -0800

    Add: Card Component

---
+++ app/components/Card.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Image from 'next/image'
+import PropTypes from 'prop-types';
+
+
+function Card(props) {
+    return (
+        <div>
+            <Image
+            src={props.src}
+            width={500}
+            height={500}
+            alt={props.alt}
+        />
+        </div>
+    )
+}
+
+Card.propTypes = {
+    src: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+};
+
+export default Card;
