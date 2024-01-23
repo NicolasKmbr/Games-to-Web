86e706c181fbbeb2ba30ad15326cdf91cab5ca4d
NicolasKmbr <braun.nicolasbraun@gmail.com>
2024-01-22 22:09:36 -0800

    Add uniqueObject.test.js and remove uniqueArray.test.js

--- app/tests/uniqueArray.test.js
+++
@@ -1,9 +0,0 @@
-const { getArray } = require('./path-to-your-function');
-
-describe('getArray function', () => {
-  test('should only contain unique items', () => {
-    const array = getArray();
-    const set = new Set(array);
-    expect(set.size).toBe(array.length);
-  });
-});
