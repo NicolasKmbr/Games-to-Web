d221743b7a6889b39502a509d80be1e3a95e80ce
NicolasKmbr <braun.nicolasbraun@gmail.com>
2024-02-02 23:14:27 -0800

    Feature: cards are revealable

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,16 +1,17 @@
-import React from 'react'
 import Image from 'next/image'
 import PropTypes from 'prop-types';
 
 
 function Card(props) {
+
     return (
         <div className='w-fit'>
             <Image
-            src={props.src}
+            src={props.revealed ? props.src : "card_back.svg"}
             width={100}
             height={100}
             alt={props.alt}
+            onClick={props.handleCardClick}
         />
         </div>
     )
@@ -19,6 +20,8 @@ function Card(props) {
 Card.propTypes = {
     src: PropTypes.string.isRequired,
     alt: PropTypes.string.isRequired,
+    revealed: PropTypes.bool,
+    handleCardClick: PropTypes.func
 };
 
 export default Card;
--- app/components/MemoryBoard.js
+++ app/components/MemoryBoard.js
@@ -1,29 +1,52 @@
-import React from 'react'
+"use client"
 import Card from '@/components/Card'
-import { GetAssetPaths } from '@/utils/GetAssetPaths'
 import { createCard } from '@/utils/CreateCard'
 import { getRandomSuit } from '@/utils/getRandomSuit'
 import { getRandomCardRank } from '@/utils/getRandomCardRank'
 import { addUniqueObject } from '@/utils/addUniqueObject'
+import { useEffect, useState } from 'react'
 
 
 
-async function MemoryBoard() {
-const cards =  new Array()
+function MemoryBoard() {
+  const [cards, setCards] = useState([]);
+  const [flip, setFlip] = useState([]);
 
-while (cards.length < 8) {
-  const card = createCard(getRandomCardRank(), getRandomSuit())
-  addUniqueObject(card, cards)
-}
+  useEffect(() => {
+    const allCards = [];
+    while (allCards.length < 8) {
+      const suit = getRandomSuit();
+      const rank = getRandomCardRank();
+      const newCard = createCard(rank, suit);
+      addUniqueObject(newCard, allCards);
+    }
+    setCards(allCards.concat(allCards));
+  }
+  , []);
 
-console.log("Cards = " + cards)
-console.log("Cards length = " + cards.length)
-const allCards = cards.concat(cards);
+  useEffect(() => {
+    setFlip(Array(cards.length).fill(false));
+  }, [cards]);
+
+
+  
+
+  const handleCardClick = (index) => {
+    const newFlip = [...flip];
+    newFlip[index] = !newFlip[index];
+    setFlip(newFlip);
+  };
+
+  console.log(cards);
+  console.log("flip array = " + flip);
+
+
+
 
   return (
-    <div className='grid grid-cols-4 w-fit m-auto'>
-      {allCards.map((card, index) => (
-                <Card key={index} src={card.imagePath} alt='Memory Card' />
+    <div className='grid grid-cols-4 w-fit m-auto gap-3'>
+      {cards.map((card, index) => (
+                <Card revealed={flip[index]} key={index} src={card.imagePath} alt='Memory Card' handleCardClick={() => handleCardClick(index)} />
             ))}
     </div>
   )
