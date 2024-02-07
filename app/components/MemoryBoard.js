f321b2ae7d210d203b744f4ca498e1c32570bfd0
NicolasKmbr <braun.nicolasbraun@gmail.com>
2024-02-06 17:55:11 -0800

    MemoryBoard: no double card clicking and max 2 cards revealed

--- app/components/MemoryBoard.js
+++ app/components/MemoryBoard.js
@@ -1,64 +1,75 @@
-"use client"
-import Card from '@/components/Card'
-import { createCard } from '@/utils/CreateCard'
-import { getRandomSuit } from '@/utils/getRandomSuit'
-import { getRandomCardRank } from '@/utils/getRandomCardRank'
-import { addUniqueObject } from '@/utils/addUniqueObject'
-import { useEffect, useState } from 'react'
-
-
+"use client";
+import Card from "@/components/Card";
+import { createCardDeck } from "@/utils/createCardDeck";
+import { useEffect, useState } from "react";
 
 function MemoryBoard() {
-  const [cards, setCards] = useState([]);
+  const [cards, setCards] = useState(createCardDeck(8, true));
   const [revealed, setRevealed] = useState([]);
   const [score, setScore] = useState(0);
 
   useEffect(() => {
-    const allCards = [];
-    while (allCards.length < 8) {
-      const suit = getRandomSuit();
-      const rank = getRandomCardRank();
-      const newCard = createCard(rank, suit);
-      addUniqueObject(newCard, allCards);
+    if (cards.every((card) => card.imagePath === "")) {
+      alert("You won!");
     }
-    setCards(allCards.concat(allCards));
-  }
-  , []);
+  } , [cards]);
 
   useEffect(() => {
-    console.log('revealed: ', revealed);
-    console.log('revealed.length: ', revealed.length);
+    console.log("revealed: ", revealed);
+    console.log("revealed.length: ", revealed.length);
+    console.log("cards[]: ", cards);
     const [first, second] = revealed;
     if (revealed.length === 2) {
-
-      if (cards[first].rank === cards[second].rank && cards[first].suit === cards[second].suit) {
+      if (
+        cards[first].rank === cards[second].rank &&
+        cards[first].suit === cards[second].suit
+      ) {
         setScore(score + 1);
+        // Remove the cards from the board
+        setCards(
+          cards.map((card, index) => {
+            if (index === first || index === second) {
+              return { ...card, imagePath: "" };
+            }
+            return card;
+          })
+        );
       }
-      setTimeout(() => {
-        setRevealed([]);
-      }, 1000);
     }
-  } , [revealed]);
+  }, [revealed]);
+
 
   const handleCardClick = (index) => {
-    console.log('index: ', index);
-    setRevealed([...revealed, index]);
+    console.log("index: ", index);
+    // If the card is already revealed, do nothing
+    if (revealed.includes(index)) {
+      return;
+    }else{
+      setRevealed([...revealed, index]);
+    }
+    if (revealed.length >= 2) {
+      setRevealed((currentArray) => currentArray.slice(2));
+    }
   };
 
-
-  //console.log(cards);
-
-
-
   return (
-    <div className='grid grid-cols-4 w-fit m-auto gap-3'>
-      {cards.map((card, index) => (
-                <Card revealed={revealed.includes(index)} key={index} src={card.imagePath} alt='Memory Card' handleCardClick={() => handleCardClick(index)} />
-            ))}
+    <div className="relative grid grid-cols-4 w-fit m-auto gap-3">
+      {cards.map((card, index) =>
+        card.imagePath ? (
+          <Card
+            revealed={revealed.includes(index)}
+            key={index}
+            src={card.imagePath}
+            alt="Memory Card"
+            handleCardClick={() => handleCardClick(index)}
+          />
+        ) : (
+          <div className='w-fit'></div>
+        )
+      )}
       <p>{score}</p>
     </div>
-  )
+  );
 }
 
-
-export default MemoryBoard
+export default MemoryBoard;
