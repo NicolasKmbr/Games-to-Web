912a2fedb42bfabe826a1b0800d563a3ffa76a4c
NicolasKmbr <braun.nicolasbraun@gmail.com>
2025-02-26 23:05:42 +0100

    Style: Centered Memory Game

--- app/memory/page.js
+++ app/memory/page.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import MemoryBoard from '../components/MemoryBoard'
-
+import React from "react";
+import MemoryBoard from "../components/MemoryBoard";
 
 function MemoryPage() {
   return (
-    <div>
-      <h1 className='text-5xl m-auto w-fit py-5'>Memory Game</h1>
+    <div className="h-screen content-center">
+      <h1 className="text-5xl m-auto w-fit py-5">Memory Game</h1>
       <MemoryBoard />
     </div>
-  )
+  );
 }
 
-export default MemoryPage
+export default MemoryPage;
